e410d82cd8221529a2e4706de037edb2076e409e
VanDarkMaster117 <130970577+VanDarkMaster117@users.noreply.github.com>
2024-05-16 16:33:05 +0800

    Add files via upload

---
+++ Curresd.cs
@@ -0,0 +1,62 @@
+﻿using DG.Tweening;
+using SWS;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Curresd : MonoBehaviour
+{
+    public splineMove first;
+    public splineMove second;
+    public FirstPersonController a;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        print(111);
+        
+
+    }
+    public void Jie()
+    {
+        a.enabled = true;
+        Debug.Log(222);
+       
+    }
+    public void Daohang()
+    {
+        a.enabled = false;
+        first.StartMove();
+    }
+    public void Daohang2()
+    {
+        a.enabled = false;
+        second.StartMove();
+    }
+    public void Fanhui()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+    public void Load()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            first.Stop();
+            second.Stop();
+
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        
+    }
+    
+}
---
+++ StartBut.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class StartBut : MonoBehaviour
+{
+    public GameObject dongfangmen;
+    public GameObject xianggeli;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dongfangmen.SetActive(false);
+        xianggeli.SetActive(false);
+    }
+    public void Loadzero()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Tuichu()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+    public void Loadone()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Loadtwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Loadfour()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Loadthree()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Loadfive()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Loadsix()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Loadseven()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void Show()
+    {
+        xianggeli.SetActive(true);
+        dongfangmen.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
